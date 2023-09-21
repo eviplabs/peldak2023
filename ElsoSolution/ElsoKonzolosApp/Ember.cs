359ab82ff5b87a61bfb95af8de7ed213ba493ae6
kristof <csorba.kristof@vik.bme.hu>
2023-09-21 14:49:02 +0200

    Ember added

---
+++ ElsoSolution/ElsoKonzolosApp/Ember.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElsoKonzolosApp
+{
+    public class Ember
+    {
+        public string Nev { get; set; }
+        public int SzuletesiEv { get; set; }
+
+        public Ember(string nev, int szuletesiEv)
+        {
+            Nev = nev;
+            SzuletesiEv = szuletesiEv;
+        }
+
+        //override public string ToString()
+        //{
+        //    return Nev + " (" + SzuletesiEv + ")";
+        //}
+    }
+}
--- ElsoSolution/ElsoKonzolosApp/Program.cs
+++ ElsoSolution/ElsoKonzolosApp/Program.cs
@@ -16,11 +16,11 @@
 
             int n = 5;
             p.NovelEsKiir(ref n);
-            Console.WriteLine("N értéke a Main-ben: "+n.ToString());
+            Console.WriteLine("N értéke a Main-ben: "+n);
 
             double? d = 5.5;
             d = null;
-            if (d.HasValue) Console.WriteLine("d="+d.ToString());
+            if (d.HasValue) Console.WriteLine("d="+d);
 
             p.Ertek = 55;
         }
@@ -41,22 +41,15 @@
                 }
             }
         }
-        //public int GetErtek()
-        //{
-        //    return _belsoErtek;
-        //}
 
-        //public void SetErtek(int ujErtek)
-        //{
-        //      _belsoErtek = ujErtek;
-        //}
+        public int Ertek2 { get; set; } // Property tömören
 
         public const int InvalidTemperature = -1000;
 
         private int NovelEsKiir(ref int szam)
         {
             szam++;
-            Console.WriteLine("Szam értéke bent: "+szam.ToString());
+            Console.WriteLine("Szam értéke bent: "+szam);
             return szam;
         }
 
