efbd72289b102a88701c4ec046609811cb261514
kristof <csorba.kristof@vik.bme.hu>
2023-09-21 15:35:00 +0200

    StringBuilder, string interpolation

--- ElsoSolution/ElsoKonzolosApp/Program.cs
+++ ElsoSolution/ElsoKonzolosApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ElsoKonzolosApp
+﻿using System.Text;
+
+namespace ElsoKonzolosApp
 {
     public class Program
     {
@@ -8,6 +10,22 @@
 
             IEmberPrinter ep = CreatePrinter(true);
             ep.Print(ember);
+
+            string x = "hello";
+            x = "hello2";
+            x = "hello" + "2" + ember.Nev + " ( " + ember.SzuletesiEv + " )";
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("hello");
+            sb.Append("2");
+            sb.Append(ember.Nev);
+            sb.Append(" ( ");
+            sb.Append(ember.SzuletesiEv);
+            sb.Append(" )");
+            x = sb.ToString();
+
+            x = $"hello2 {ember.Nev} ( {ember.SzuletesiEv} )";
+
         }
 
         private static IEmberPrinter CreatePrinter(bool isDesign)   // Factory method
