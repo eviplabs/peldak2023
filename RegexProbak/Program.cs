9fa4988c412cfaa83e035c4f6bf23f4d07306044
Kristof <kristof@aut.bme.hu>
2023-09-25 14:19:54 +0200

    RegEx example

--- RegexProbak/Program.cs
+++ RegexProbak/Program.cs
@@ -7,7 +7,17 @@ namespace RegexProbak
     {
         static void Main(string[] args)
         {
+            var lines = File.ReadAllLines("Szoveg.txt");
 
+            Regex regex = new Regex(@"^BME.+,(\d+/\d+/\d+),.+$");
+            foreach (var line in lines)
+            {
+                MatchCollection matches = regex.Matches(line);
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.Groups[1].Value);
+                }
+            }
         }
     }
 }
