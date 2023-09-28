b889813782c54f6b757f87e4b6cb90c04cd16a06
kristof <csorba.kristof@vik.bme.hu>
2023-09-28 15:01:11 +0200

    Regex based sum of student counts in Neptun export

--- RegexProbak/Program.cs
+++ RegexProbak/Program.cs
@@ -7,6 +7,24 @@ namespace RegexProbak
     {
         static void Main(string[] args)
         {
+            string filePath = @"Szoveg.txt";
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                int sum = 0;
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var pattern = @"^(?:[^,]*,){10}(\d+)/(\d+)/(\d+),";
+                    var regex = new Regex(pattern);
+                    var match = regex.Match(line);
+                    if (match.Success)
+                    {
+                        var studentCount = int.Parse(match.Groups[1].Value);
+                        sum += studentCount;
+                    }
+                }
+                Console.WriteLine(sum);
+            }
         }
     }
 }
