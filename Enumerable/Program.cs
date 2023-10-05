200a2a5d22735818294206f93b2bbf7e830fbde1
kristof <csorba.kristof@vik.bme.hu>
2023-10-05 14:45:49 +0200

    Enumerable initial commit

---
+++ Enumerable/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Enumerable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = GetNumbers(10);
+            foreach(var number in numbers)
+            {
+                System.Console.WriteLine(number);
+            }
+        }
+
+        static public int[] GetNumbers(int count)
+        {
+            int[] numbers = new int[count];
+            for(int i = 0; i < count; i++)
+            {
+                numbers[i] = i;
+            }
+            return numbers;
+        }
+    }
+}
