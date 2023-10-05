52c51313cd77881b2008919decd5520eb377eeb6
kristof <csorba.kristof@vik.bme.hu>
2023-10-05 15:11:46 +0200

    Take2 implemented

---
+++ Enumerable/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerable
+{
+    internal static class ExtensionMethods
+    {
+        public static IEnumerable<int> Take2(this IEnumerable<int> source)
+        {
+            int counter = 0;
+            foreach (var item in source)
+            {
+                if (counter >= 2)
+                    yield return item;
+                counter = (counter + 1) % 4;
+            }
+        }
+    }
+}
--- Enumerable/Program.cs
+++ Enumerable/Program.cs
@@ -4,17 +4,20 @@
     {
         static void Main(string[] args)
         {
-            var numbers = GetNumbers(10);
-            var enumerator = numbers.GetEnumerator();
-            //foreach(var number in numbers)
-            //{
-            //    System.Console.WriteLine(number);
-            //}
-            while(enumerator.MoveNext())
+            var numbers = GetPrimes(10);
+            //var enumerator = numbers.GetEnumerator();
+
+            //ExtensionMethods.Take2(numbers);
+            //numbers.Take2();
+            foreach (var number in numbers.Take2())
             {
-                var number = enumerator.Current;
-                Console.WriteLine(number);
+                System.Console.WriteLine(number);
             }
+
+            Console.WriteLine( numbers.Sum() );
+
+            Console.WriteLine(numbers.Take2().Sum());
+
         }
 
         static public IEnumerable<int> GetNumbers(int count)
@@ -23,6 +26,45 @@
             {
                 yield return i;
             }
+        }
+
+        // GetPrimes
+        static public IEnumerable<int> GetPrimes(int count)
+        {
+            int counter = 0;
+            int current = 2;
+            while(counter < count)
+            {
+                if (IsPrime(current))
+                {
+                    yield return current;
+                    counter++;
+                }
+                current++;
+            }
+        }
+
+        private static bool IsPrime(int i)
+        {
+            bool isPrime = true;
+
+            if (i == 1 || i == 0)
+            {
+                isPrime = false;
+            }
+            else
+            {
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            return isPrime;
         }
     }
 }
