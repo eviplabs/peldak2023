a7f635c4200d87b1e657bab57fee2194001e5c10
kristof <csorba.kristof@vik.bme.hu>
2023-10-12 14:48:03 +0200

    3. feladat

---
+++ ZH1_2022/Tests/ModifierTests.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class ModifierTests
+    {
+        [Fact]
+        public void TestGrowPerson()
+        {
+            var p = new Person("John", 77, 170);
+            var grower = new PersonGrower();
+            grower.N = 5;
+            grower.Modify(p);
+            Assert.Equal(175, p.Height);
+        }
+    }
+}
