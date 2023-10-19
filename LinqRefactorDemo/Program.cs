c0b703f246336fa46ae4bb8b1776d8f9710adcba
kristof <csorba.kristof@vik.bme.hu>
2023-10-19 15:17:25 +0200

    Linq refactor ready

--- LinqRefactorDemo/Program.cs
+++ LinqRefactorDemo/Program.cs
@@ -1,57 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-using LinqRefactorDemo;
+﻿using LinqRefactorDemo;
 
-string[] csvlines = File.ReadAllLines("rooms.csv");
-var rooms = new List<Room>();
-foreach(string line in csvlines)
-{
-    string[] parts = line.Split(',');
-    string name = parts[0];
-    string capacity = parts[1];
-    string faculty = parts[2];
-    string needsKey = parts[3];
-    string hasProjector = parts[4];
-
-    var room = new Room();
-    room.Name = name;
-    room.Capacity = int.Parse(capacity);
-    room.Faculty = faculty;
-    room.NeedsKey = needsKey.Contains("kulcs");
-    room.HasProjector = hasProjector.Contains("proje");
-    rooms.Add(room);
-
-    Console.WriteLine($"Room {room.Name} has capacity {room.Capacity}. Faculty {room.Faculty}, needskey {room.NeedsKey}, hasProjector {room.HasProjector}");
-}
-
-List<string> faculties = new List<string>();
-foreach(Room room in rooms)
-{
-    if(!faculties.Contains(room.Faculty))
+var facultiesWithCapacity =
+    File.ReadAllLines("rooms.csv")
+    .Select(csvline => csvline.Split(','))
+    .Select(parts => new Room
     {
-        faculties.Add(room.Faculty);
-    }
-}
-
-// Total capacity of each faculty with rooms having projector
-var facultiesWithCapacity = new List<FacultyWithCapacity>();
-foreach(string faculty in faculties)
-{
-    int sumCapacityOfFaculty = 0;
-    foreach (Room room in rooms)
-    {
-        if(room.Faculty == faculty && room.HasProjector)
-        {
-            sumCapacityOfFaculty += room.Capacity;
-        }
-    }
-    facultiesWithCapacity.Add(new FacultyWithCapacity
-        { Name = faculty, Capacity = sumCapacityOfFaculty });
-}
-
-facultiesWithCapacity.Sort(new FacultyCapacityComparer());
+        Name = parts[0],
+        Capacity = int.Parse(parts[1]),
+        Faculty = parts[2],
+        NeedsKey = parts[3].Contains("kulcs"),
+        HasProjector = parts[4].Contains("proje")
+    })
+    .Where(room => room.HasProjector)
+    .GroupBy(rooms => rooms.Faculty)
+    .Select(group => (Name: group.Key, Capacity: group.Sum(room => room.Capacity)))
+    .OrderBy(f => f.Capacity);
 
 foreach(var faculty in facultiesWithCapacity)
 {
     Console.WriteLine($"Faculty {faculty.Name} has capacity (with projector) {faculty.Capacity}");
 }
-
