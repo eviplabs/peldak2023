971d0aaca18fae393a33951de5404a28fada4125
kristof <csorba.kristof@vik.bme.hu>
2023-10-26 15:19:37 +0200

    Button color taken with x:Static

--- FirstMauiApp/FirstMauiApp/MainPage.xaml.cs
+++ FirstMauiApp/FirstMauiApp/MainPage.xaml.cs
@@ -4,6 +4,8 @@
     {
         int count = 0;
 
+        public static readonly Color InitialButtonColor = Colors.Red;
+
         public MainPage()
         {
             InitializeComponent();
@@ -18,7 +20,26 @@
             else
                 CounterBtn.Text = $"Clicked {count} times";
 
+            CounterBtn.BackgroundColor = Color.FromRgb(10*count % 255, count % 255, count % 255);
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void AddNewButtonButton_Clicked(object sender, EventArgs e)
+        {
+            Button newButton = new Button()
+            {
+                Text = "Hello",
+                BackgroundColor = InitialButtonColor
+            };
+            newButton.Clicked += OnHelloButtonClicked;
+
+            ColorButtons.Children.Add(newButton);
+        }
+
+        private void OnHelloButtonClicked(object sender, EventArgs e)
+        {
+            Button senderButton = (Button)sender;
+            senderButton.BackgroundColor = Colors.Aqua;
         }
     }
 }
