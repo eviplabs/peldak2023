1632772a6d1eb1da064688b44886df7967c96fa7
Kristof <kristof@aut.bme.hu>
2023-11-02 13:43:20 +0100

    MauiBindingDemo intial version

---
+++ MauiBindingDemo/MauiBindingDemo/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBindingDemo
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public int Age { get; set; }
+    }
+}
